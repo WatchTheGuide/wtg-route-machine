ac2cbc90de7cdf6d10133df93356921fa6b9b925
Szymon Walczak <szymon@watchtheguide.com>
2025-12-04 20:58:34 +0100

    fix: Simplify tabs to match Ionic documentation
    - Use single icon per tab (Ionic handles selected state automatically)
    - Move Redirect inside IonRouterOutlet
    - Simplify CSS using standard Ionic selectors
    - Increase icon size to 26px for better visibility

--- frontend-ionic/src/pages/Tabs.tsx
+++ frontend-ionic/src/pages/Tabs.tsx
@@ -8,16 +8,7 @@ import {
   IonTabButton,
   IonTabs,
 } from '@ionic/react';
-import {
-  homeOutline,
-  home,
-  mapOutline,
-  map,
-  listOutline,
-  list,
-  settingsOutline,
-  settings,
-} from 'ionicons/icons';
+import { home, map, list, settings } from 'ionicons/icons';
 
 import Home from './HomePage';
 import MapPage from './MapPage';
@@ -30,6 +21,7 @@ const Tabs: React.FC = () => {
   return (
     <IonTabs>
       <IonRouterOutlet>
+        <Redirect exact path="/tabs" to="/tabs/home" />
         <Route exact path="/tabs/home">
           <Home />
         </Route>
@@ -42,33 +34,26 @@ const Tabs: React.FC = () => {
         <Route exact path="/tabs/settings">
           <Settings />
         </Route>
-        <Route exact path="/tabs">
-          <Redirect to="/tabs/home" />
-        </Route>
       </IonRouterOutlet>
 
-      <IonTabBar slot="bottom" className="app-tab-bar">
+      <IonTabBar slot="bottom">
         <IonTabButton tab="home" href="/tabs/home">
-          <IonIcon icon={homeOutline} className="tab-icon-inactive" />
-          <IonIcon icon={home} className="tab-icon-active" />
+          <IonIcon icon={home} />
           <IonLabel>Trasy</IonLabel>
         </IonTabButton>
 
         <IonTabButton tab="map" href="/tabs/map">
-          <IonIcon icon={mapOutline} className="tab-icon-inactive" />
-          <IonIcon icon={map} className="tab-icon-active" />
+          <IonIcon icon={map} />
           <IonLabel>Mapa</IonLabel>
         </IonTabButton>
 
         <IonTabButton tab="instructions" href="/tabs/instructions">
-          <IonIcon icon={listOutline} className="tab-icon-inactive" />
-          <IonIcon icon={list} className="tab-icon-active" />
+          <IonIcon icon={list} />
           <IonLabel>Instrukcje</IonLabel>
         </IonTabButton>
 
         <IonTabButton tab="settings" href="/tabs/settings">
-          <IonIcon icon={settingsOutline} className="tab-icon-inactive" />
-          <IonIcon icon={settings} className="tab-icon-active" />
+          <IonIcon icon={settings} />
           <IonLabel>Ustawienia</IonLabel>
         </IonTabButton>
       </IonTabBar>
